4404c47f1088c09e2d5c5097f4753a39e86dfb57
hirokinakajima2020 <hiro312.n@docomo.ne.jp>
2020-07-31 18:01:21 +0900

    a

--- mydb.sql
+++ mydb.sql
@@ -1,37 +1,63 @@
-# (A1)mydbというデータベースが既にあるなら削除する（危険）．
-drop database if exists mydb;
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
 
-# (A2)mydbというデータベースを作る．
-create database mydb charset=utf8mb4;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-# (A3)ユーザ名testuser，パスワードpassでmydbにアクセスできるようにする．
-grant all on mydb.* to testuser@localhost identified by 'pass';
+--
+-- Table structure for table `table1`
+--
 
-# (A4)mydbを使う．
-use mydb;
+DROP TABLE IF EXISTS `table1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `table1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `a` varchar(40) NOT NULL,
+  `b` varchar(40) NOT NULL,
+  `c` varchar(40) NOT NULL,
+  `d` varchar(40) NOT NULL,
+  `e` varchar(40) NOT NULL,
+  `f` varchar(40) NOT NULL,
+  `g` varchar(40) NOT NULL,
+  `h` varchar(40) NOT NULL,
+  `i` varchar(40) NOT NULL,
+  `j` varchar(40) NOT NULL,
+  `k` varchar(40) NOT NULL,
+  `l` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-# (A5)table1というテーブルが既にあるなら削除する（危険）．
-drop table if exists table1;
+--
+-- Dumping data for table `table1`
+--
 
-create table table1 (
-  id int primary key auto_increment, # ここはいつも同じ
-  name varchar(40) not null,
-  a varchar(40) not null,
-  b varchar(40) not null,
-  c varchar(40) not null,
-  d varchar(40) not null,
-  e varchar(40) not null,
-  f varchar(40) not null,
-  g varchar(40) not null,
-  h varchar(40) not null,
-  i varchar(40) not null,
-  j varchar(40) not null,
-  k varchar(40) not null,
-  l varchar(40) not null# 最後にはカンマがないことに注意．
-);
+LOCK TABLES `table1` WRITE;
+/*!40000 ALTER TABLE `table1` DISABLE KEYS */;
+INSERT INTO `table1` VALUES (1,'2020001','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み','提出済み'),(2,'2020002','提出済み','提出済み','提出済み','提出済み','提出済み','未提出','未提出','未提出','未提出','未提出','未提出','未提出'),(3,'2020003','未提出','未提出','未提出','未提出','未提出','未提出','未提出','未提出','未提出','未提出','未提出','未提出');
+/*!40000 ALTER TABLE `table1` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-# (C1)データを作成する．
-insert into table1 (id, name, a, b, c, d, e, f, g, h, i, j, k, l) values
-(1, '2020001', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み'),
-(2, '2020002', '提出済み', '提出済み', '提出済み', '提出済み', '提出済み', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出'),
-(3, '2020003', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出', '未提出');
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-31 18:01:04
